8d746552c2a918d0d3cebbbf88c36d2fc6c0a193
Hector Turla <turlahector@gmail.com>
2026-07-18 07:34:08 +0800

    task: add public game ellie

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const elliGameRoutePlugin = () => ({
+  name: 'elli-game-route-rewrite',
+  configureServer(server) {
+    server.middlewares.use((req, _res, next) => {
+      if (req.url === '/elli-game' || req.url === '/elli-game/') {
+        req.url = '/elli-game.html'
+      }
+      next()
+    })
+  },
+  configurePreviewServer(server) {
+    server.middlewares.use((req, _res, next) => {
+      if (req.url === '/elli-game' || req.url === '/elli-game/') {
+        req.url = '/elli-game.html'
+      }
+      next()
+    })
+  },
+})
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [elliGameRoutePlugin(), react()],
   server: {
     host: true,
     port: 5173,
